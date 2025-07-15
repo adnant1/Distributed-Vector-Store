a309fb9fe40bb7111b6056f8107aea0bb3791b30
adnant1 <adnantayib8@gmail.com>
2025-07-14 22:50:10 -0400

    feat: Structure of the controller for a single node

---
+++ vector-store-node/node/src/main/java/com/adnant1/node/controller/NodeController.java
@@ -0,0 +1,34 @@
+package com.adnant1.node.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adnant1.node.model.SearchResult;
+import com.adnant1.node.service.VectorStoreService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/node")
+public class NodeController {
+    
+    @Autowired
+    private VectorStoreService vectorStoreService;
+
+    // Index text and its embedding
+    @PostMapping("/index")
+    public void index(@RequestBody Map<String, String> body) {
+    
+    }
+
+    // Search for similar texts given a query
+    @PostMapping("/query")
+    public List<SearchResult> search(@RequestBody Map<String, Object> body) {
+
+    }
+}
