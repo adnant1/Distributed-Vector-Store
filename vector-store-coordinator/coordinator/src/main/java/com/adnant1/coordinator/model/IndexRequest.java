3c7289530bc0c3de325e0339bb168cfc71203eee
adnant1 <adnantayib8@gmail.com>
2025-07-16 22:02:06 -0400

    feat: DTO for request to /node/index

---
+++ vector-store-coordinator/coordinator/src/main/java/com/adnant1/coordinator/model/IndexRequest.java
@@ -0,0 +1,29 @@
+package com.adnant1.coordinator.model;
+
+public class IndexRequest {
+    private String id;
+    private String text;
+
+    public IndexRequest() {}
+
+    public IndexRequest(String id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
