ebc9104b3ba9d8e144f43bd3a3e2bef090b428bd
adnant1 <adnantayib8@gmail.com>
2025-07-16 23:08:36 -0400

    feat: Controller for Coordinator endpoints

---
+++ vector-store-coordinator/coordinator/src/main/java/com/adnant1/coordinator/controller/CoordinatorController.java
@@ -0,0 +1,36 @@
+package com.adnant1.coordinator.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adnant1.coordinator.model.IndexRequest;
+import com.adnant1.coordinator.model.QueryRequest;
+import com.adnant1.coordinator.model.QueryResult;
+import com.adnant1.coordinator.service.CoordinatorService;
+
+@RestController
+@RequestMapping("/coordinator")
+public class CoordinatorController {
+    
+    private final CoordinatorService coordinatorService;
+
+    public CoordinatorController(CoordinatorService coordinatorService) {
+        this.coordinatorService = coordinatorService;
+    }
+
+    // Send index request to coordinator service, which routes it to the appropriate vector node
+    @PostMapping("/index")
+    public ResponseEntity<Void> index(IndexRequest request) {
+
+    }
+
+    // Send query request to coordinator service, which queries all vector nodes and aggregates results
+    @PostMapping("/query")
+    public List<QueryResult> query(QueryRequest request) {
+    
+    }
+}
